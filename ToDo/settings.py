263a975f5c5c549378b3728b8c4e4b08f6df3b32
MKoriya <meetkoriya13@gmail.com>
2021-08-04 21:47:12 +0530

    heroku configuration

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
